63dc9c8e64cd1939bae8027d1070271dc68078cf
hiroyuki <hiroyuki@norn.niflheimr.org>
2011-03-02 22:37:02 +0900

    s/Hatena/Xatena/ その他細かい修正

--- cmd_slideshow.pl
+++ cmd_slideshow.pl
@@ -1,37 +1,17 @@
+#!/usr/bin/env perl
 use strict;
 use warnings;
 use utf8;
-use Text::Hatena;
+use Text::Xatena;
 use Encode;
 
-{
-    no strict 'refs';
-    no warnings 'redefine';
-    *{"Text::Hatena\::super_pre"} = sub {
-        my $class = shift;
-        my $items = shift->{items};
-        my $item = $items->[0] or return;
-        $item =~ s/^.*?>|(\w+)|$/$1/;
-        my $filter = $1 || $item;
-        my $texts = $class->expand($items->[1]);
-        return "<pre name=\"code\" class=\"$filter\">\n$texts</pre>\n";
-    };
-}
-
-my $header = q{
-<html>
-    <title>sldideshow</title>
-    <body>
-};
-
-my $footer = q{
-    </body>
-</html>
-};
+my $thx    = Text::Xatena->new;
+my $header = q{<html><title>sldideshow</title><body>}; 
+my $footer = q{</body></html>}; 
 
 sub parse {
     my $text = shift;
-    my $html = Text::Hatena->parse($text);
+    return $thx->format($text);
 }
 
 sub render {
@@ -45,19 +25,21 @@ sub render {
     close $fh;
 };
 
-my @text;
-my $id = 0;
-while(my $line = decode('utf8',<>) ) {
-    if( $line =~ /^__NEXT__$/ ) {
-        render( parse( join("",@text),), ++$id );                
+sub main {
+    my @text;
+    my $id = 1;
+    while(my $line = decode('utf8',<>) ) {
+        if( $line =~ /^__NEXT__$/ ) {
+            render( parse( join("",@text),), $id++ );                
+        }
+        else {
+            push @text,$line;
+        }
     }
-    else {
-        push @text,$line;
+
+    if( @text ) {
+        render( parse( join("",@text),), $id );                
     }
 }
 
-if( @text ) {
-    render( parse( join("",@text),), $id );                
-}
-
-
+&main();
