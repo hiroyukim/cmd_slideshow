b8ca48808836562daeea8d74485e6cf6bdd7640d
hiroyuki <hiroyuki@norn.niflheimr.org>
2011-03-03 23:27:49 +0900

    template file 対応 自動ディレクトリ生成対応

--- cmd_slideshow.pl
+++ cmd_slideshow.pl
@@ -4,10 +4,18 @@ use warnings;
 use utf8;
 use Text::Xatena;
 use Encode;
+use FindBin;
+use Path::Class;
+use File::HomeDir;
+use Text::Xslate;
 
-my $thx    = Text::Xatena->new;
-my $header = q{<html><title>sldideshow</title><body>}; 
-my $footer = q{</body></html>}; 
+binmode STDIN, ':utf8';
+
+my $dir          = dir(File::HomeDir->my_home, ".cmd_slideshow"); 
+my $template     = 'template.html';
+my $output_dir   = dir($FindBin::Bin,'slideshow');
+my $thx          = Text::Xatena->new;
+my $tx           = Text::Xslate->new( syntax => 'TTerse', path => [$dir]);
 
 sub parse {
     my $text = shift;
@@ -15,31 +23,43 @@ sub parse {
 }
 
 sub render {
-    my $html = shift;
-    my $id   = shift;
+    my ($html,$id,$total) = @_;
 
-    $html = $header . $html . $footer;
-
-    open my $fh, '>', $id . '.html';
-    print $fh encode('utf8',$html);
+    open my $fh, '>:utf8', "$output_dir/$id.html";
+    my $string = $tx->render(
+        $template,
+        { 
+            total => $total, 
+            page => $id,
+            text => $html, 
+        }
+    );
+    print $fh $string; 
     close $fh;
 };
 
-sub main {
-    my @text;
-    my $id = 1;
-    while(my $line = decode('utf8',<>) ) {
-        if( $line =~ /^__NEXT__$/ ) {
-            render( parse( join("",@text),), $id++ );                
-        }
-        else {
-            push @text,$line;
-        }
+sub init {
+    unless( -e $dir ){
+        die "please mkdir $dir";
     }
 
-    if( @text ) {
-        render( parse( join("",@text),), $id );                
+    unless( -e $output_dir ) {
+        mkdir $output_dir;
     }
 }
 
-&main();
+sub main {
+
+    init();
+
+    my $text  = join('', <STDIN>);
+    my $total = ($text =~ /^(__NEXT__)$/mg ) + 1;
+
+    my $id = 1;
+    for my $line ( split /^__NEXT__$/m, $text ) { 
+        render( parse( $line ), $id++, $total );                
+    }
+
+}
+
+main();
